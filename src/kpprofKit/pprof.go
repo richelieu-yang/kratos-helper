b1399c159d971d9b9bf7061c2cc132927e151cb4
Richelieu <richelieu042@gmail.com>
2024-11-01 13:26:02 +0800

    init

--- src/kpprofKit/pprof.go
+++ src/kpprofKit/pprof.go
@@ -1,1 +1,37 @@
 package kpprofKit
+
+import (
+	_ "net/http/pprof"
+
+	khttp "github.com/go-kratos/kratos/v2/transport/http"
+	"net/http"
+	"net/http/pprof"
+)
+
+func init() {
+	/*
+		net/http/pprof的 init() 会自动注册一些路由到默认的 HTTP 路由器中.
+		此处代码的目的: 取消这些自动绑定的路由.
+	*/
+	http.DefaultServeMux = http.NewServeMux()
+}
+
+// RegisterPprof 注册pprof相关的路由.
+/*
+!!!: 想调用此函数，需要先在 main.go 中匿名导入(import _)此package.
+
+参考了官方example: https://github.com/go-kratos/examples/blob/main/http/pprof/main.go
+*/
+func RegisterPprof(s *khttp.Server) {
+	s.HandleFunc("/debug/pprof", pprof.Index)
+	s.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	s.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	s.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	s.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	s.HandleFunc("/debug/allocs", pprof.Handler("allocs").ServeHTTP)
+	s.HandleFunc("/debug/block", pprof.Handler("block").ServeHTTP)
+	s.HandleFunc("/debug/goroutine", pprof.Handler("goroutine").ServeHTTP)
+	s.HandleFunc("/debug/heap", pprof.Handler("heap").ServeHTTP)
+	s.HandleFunc("/debug/mutex", pprof.Handler("mutex").ServeHTTP)
+	s.HandleFunc("/debug/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
+}
