37d6bf6e99a1a1f1b539172c23bb2e1c93c7ece4
Richelieu <richelieu042@gmail.com>
2024-11-02 10:16:09 +0800

    init

--- src/http/kcorsKit/validator_test.go
+++ src/http/kcorsKit/validator_test.go
@@ -12,6 +12,11 @@ func TestNewValidator(t *testing.T) {
 	}
 
 	{
+		v := NewValidator([]string{"", ""})
+		fmt.Println(v.ValidateOrigin("https://www.baidu.com")) // true
+	}
+
+	{
 		v := NewValidator([]string{"*.baidu.com"})
 		fmt.Println(v.ValidateOrigin("http://www.baidu.com"))  // true
 		fmt.Println(v.ValidateOrigin("https://www.baidu.com")) // true
