8c82c28b444cb05e34e1bb43a034e86aed0503f6
Richelieu <richelieu042@gmail.com>
2024-11-02 10:11:35 +0800

    init

--- src/http/kcorsKit/cors.go
+++ src/http/kcorsKit/cors.go
@@ -3,14 +3,16 @@ package kcorsKit
 import "github.com/go-kratos/kratos/v2/transport/http"
 import "github.com/gorilla/handlers"
 
-func NewCorsFilter(allowOrigins []string) http.ServerOption {
-	return http.Filter(NewCorsFilterFunc(allowOrigins))
+func NewCorsServerOption(allowOrigins []string) http.ServerOption {
+	filterFunc := NewCorsFilterFunc(allowOrigins)
+	return http.Filter(filterFunc)
 }
 
 // NewCorsFilterFunc
 /*
 @param allowOrigins: 允许的跨域域名 	(1) 支持域名匹配
 									(2) 可以为nil，即都允许（并非通过 "*" 来实现）
+									(3) 如果有一元素为"*"，即都允许（并非通过 "*" 来实现）
 */
 func NewCorsFilterFunc(allowOrigins []string) http.FilterFunc {
 	validator := NewValidator(allowOrigins)
