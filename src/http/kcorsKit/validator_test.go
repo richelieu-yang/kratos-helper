b15e31791405f2eefd2708affd6ff394815ad826
Richelieu <richelieu042@gmail.com>
2024-11-02 09:55:34 +0800

    init

---
+++ src/http/kcorsKit/validator_test.go
@@ -0,0 +1,25 @@
+package kcorsKit
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewValidator(t *testing.T) {
+	{
+		v := NewValidator([]string{})
+		fmt.Println(v.ValidateOrigin("https://www.baidu.com")) // true
+	}
+
+	{
+		v := NewValidator([]string{"*.baidu.com"})
+		fmt.Println(v.ValidateOrigin("http://www.baidu.com"))  // true
+		fmt.Println(v.ValidateOrigin("https://www.baidu.com")) // true
+	}
+
+	{
+		v := NewValidator([]string{"https://*.baidu.com"})
+		fmt.Println(v.ValidateOrigin("http://www.baidu.com"))  // false
+		fmt.Println(v.ValidateOrigin("https://www.baidu.com")) // true
+	}
+}
